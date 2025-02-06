93bace5fd7e01b28f88b5c9b7c749b036f24cd6c
Mohamad <zolfagharipour@gmail.com>
2025-02-06 18:01:09 +0100

    works

---
+++ chat/consumers.py
@@ -0,0 +1,26 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+        await self.send(text_data=json.dumps({"message": "Connected to chatbot!"}))
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        user_message = data.get("message", "")
+
+        bot_response = self.get_response(user_message)
+
+        await self.send(text_data=json.dumps({"response": bot_response}))
+
+    async def disconnect(self, close_code):
+        print("WebSocket disconnected")
+
+    def get_response(self, message):
+        responses = {
+            "hello": "Hello! How can I help you?",
+            "bye": "Goodbye! Have a nice day!",
+            "help": "My consciousness is so limited, i only can understand hello and bye :(",
+        }
+        return responses.get(message.lower(), "I don't understand that.")
